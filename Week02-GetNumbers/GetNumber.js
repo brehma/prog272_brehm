bda0f6d95cc3457d330c290ef973027135ae6272
Adam Brehm <adambrehm.bc@gmail.com>
2015-01-27 02:11:31 +0000

    Who knows

---
+++ Week02-GetNumbers/GetNumber.js
@@ -0,0 +1,34 @@
+var getNumbers;
+
+getNumbers = {
+
+    getOne: function () {
+        'use strict';
+        return 1;
+    }
+};
+
+getNumbers = {
+    
+    getTwo: function () {
+        'use strict';
+        return 2;
+    }
+};
+
+getNumbers = {
+    
+    getThree: function () {
+        'use strict';
+        return 3;
+        
+    }
+};
+
+getNumbers = {
+    
+    getFour: function () {
+        'use strict';
+        return 4;
+    }
+};
